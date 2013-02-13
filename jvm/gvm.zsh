e22555749725880404c80bbfbac92d3dfed584cf
samcday <sam.c.day@gmail.com>
2013-02-13 14:02:18 +1000

    Added node/npm.zsh for global node_modules pathing. Added jvm/gvm.zsh for gvm pathing, added jvm/home.zsh for JAVA_HOME.

---
+++ jvm/gvm.zsh
@@ -0,0 +1,1 @@
+[[ -s "$HOME/.gvm/bin/gvm-init.sh" && -z $(which gvm-init.sh | grep '/gvm-init.sh') ]] && source "$HOME/.gvm/bin/gvm-init.sh"
---
+++ jvm/home.zsh
@@ -0,0 +1,2 @@
+# TODO: platform specific config for OSX/ubuntu.
+export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
