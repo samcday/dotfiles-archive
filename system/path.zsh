6a2a58ea10a3f2f443d7a3ccf79fde70f308c074
samcday <sam.c.day@gmail.com>
2013-02-14 14:59:02 +1000

    added platform specific bin/linux and bin/osx dirs to PATH. Made subl work on git commits under linux.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,11 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH_DF/bin:$PATH"
-
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+case $OSTYPE in
+darwin*)
+	export PATH="$ZSH_DF/bin/osx:$PATH"
+	;;
+linux*)
+	export PATH="$ZSH_DF/bin/linux:$PATH"
+	;;
+esac
