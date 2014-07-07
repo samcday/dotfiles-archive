d00b06915109d7a97a6246b88c6b22b999174e79
Sam Day <sday@atlassian.com>
2014-07-07 11:42:46 +1000

    Update virtualenvwrapper

--- python/virtualenvwrapper.zsh
+++ python/virtualenvwrapper.zsh
@@ -1,2 +1,3 @@
 [[ -s "/usr/local/bin/virtualenvwrapper.sh" ]] && source /usr/local/bin/virtualenvwrapper.sh
 [[ -s "/usr/local/share/python/virtualenvwrapper.sh" ]] && source /usr/local/share/python/virtualenvwrapper.sh
+export WORKON_HOME=~/.myvirtualenvs
