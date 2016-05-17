d80143ca2ace5df747c82775cee6dac332a9059e
Sam Day <me@samcday.com.au>
2016-05-17 14:41:34 -0700

    nvm: automatically use nvm in directories

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,7 +1,10 @@
 autoload -U add-zsh-hook
 load-nvmrc() {
+  current_node=$(nvm current)
   if [[ -f .nvmrc && -r .nvmrc ]]; then
-    nvm use
+    if [[ "$(nvm current)" != "$(nvm version `cat .nvmrc`)" ]]; then
+      nvm use
+    fi
   fi
 }
 add-zsh-hook chpwd load-nvmrc
